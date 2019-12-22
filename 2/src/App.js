9e005c08559602f8074f27075ed62f4b218aa2f7
SebastianWesolowski <sebastian.wesolowski.sw@gmail.com>
2019-12-22 10:26:01 +0100

    add git ignore and styled form

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,45 +1,58 @@
-import React from 'react';
-import ListWrapper from './components/ListWrpapper/ListWraper';
+import React from "react";
+import ListWrapper from "./components/ListWrpapper/ListWraper";
+import Form from "./components/Form/Form";
 
-import './index.css';
+import "./index.css";
 const initialStateItems = [
-    {
-        image: 'https://picsum.photos/150/150?random=1',
-        name: 'Dan Abramov',
-        twitterLink: 'https://twitter.com/dan_abramov',
-    },
-    {
-        image: 'https://picsum.photos/150/150?random=2',
-        name: 'Ryan Florence',
-        description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
-        twitterLink: 'https://twitter.com/ryanflorence',
-    },
-    {
-        image: 'https://picsum.photos/150/150?random=3',
-        name: 'Michael Jackson',
-        description: 'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
-        twitterLink: 'https://twitter.com/mjackson',
-    },
-    {
-        image: 'https://picsum.photos/150/150?random=4',
-        name: 'Kent C. Dodds',
-        description: 'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
-        twitterLink: 'https://twitter.com/kentcdodds',
-    }];
+  {
+    image: "https://picsum.photos/150/150?random=1",
+    name: "Dan Abramov",
+    twitterLink: "https://twitter.com/dan_abramov"
+  },
+  {
+    image: "https://picsum.photos/150/150?random=2",
+    name: "Ryan Florence",
+    description:
+      "Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.",
+    twitterLink: "https://twitter.com/ryanflorence"
+  },
+  {
+    image: "https://picsum.photos/150/150?random=3",
+    name: "Michael Jackson",
+    description:
+      "Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.",
+    twitterLink: "https://twitter.com/mjackson"
+  },
+  {
+    image: "https://picsum.photos/150/150?random=4",
+    name: "Kent C. Dodds",
+    description:
+      "Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS",
+    twitterLink: "https://twitter.com/kentcdodds"
+  }
+];
 class App extends React.Component {
-    state = {
-        items: [...initialStateItems],
+  state = {
+    items: [...initialStateItems]
+  };
 
-    };
-
-    render() {
-        return (
-            <div>
-                <ListWrapper items={this.state.items} />
-            </div>
-
-        )
-    }
+  addItem = e => {
+    e.preventDefault();
+    console.log(e.target);
+    console.log(e.target[0].value);
+    console.log(e.target[1].value);
+    console.log(e.target[2].value);
+    console.log(e.target[3].value);
+    console.log(e.target[4].value);
+  };
+  render() {
+    return (
+      <div>
+        <ListWrapper items={this.state.items} />
+        <Form submitFn={this.addItem} />
+      </div>
+    );
+  }
 }
 
 export default App;
